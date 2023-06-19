40f603a667231c9c18dac762699364041da3fd87
Burak Güllüler <burakgulluler@gmail.com>
2023-06-19 19:45:41 +0300

    basic ui

---
+++ src/components/Product.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Card, Button } from "react-bootstrap";
+
+const Product = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    //api
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => setProducts(result));
+  }, []);
+
+  const cards = products.map((product) => (
+    <div className="col-md-2" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>${product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: "white" }}>
+          <Button variant="primary">Add To Card</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
+  return (
+    <>
+      <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
+
+export default Product;
