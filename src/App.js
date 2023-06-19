0163e303dfbfc3de2282c7371d2237cd330cdc5d
Burak Güllüler <burakgulluler@gmail.com>
2023-06-19 20:12:34 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-import Product from './components/Product';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Cart from "./components/Cart";
+import Dashboard from "./components/Dashboard";
+import Product from "./components/Product";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./components/RootLayout";
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Dashboard />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+    </Route>
+  ))
+
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router}/>
     </div>
   );
 }
